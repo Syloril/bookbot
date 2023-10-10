6f5522e9ae2c80b2ae29941949f538faa80ee852
Syloril <enriquegonzalez1031@gmail.com>
2023-10-10 13:48:12 -0500

    create method to get character count of text file

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    num_of_words = word_count(text)
-    print(text)
+    num_of_words = get_word_count(text)
+    number_for_letter = get_letter_count(text)
     print(num_of_words)
+    print(number_for_letter)
+
+def get_word_count(text):
+    lines = text.split()
+    return len(lines)
+
+def get_letter_count(text):
+    letters = {}
+    for letter in text:
+        lowered = letter.lower()
+        if lowered in letters:
+            letters[lowered] += 1
+        else:
+            letters[lowered] = 1
+    return letters
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-    
-def word_count(text):
-    lines = text.split()
-    return len(lines)
+
     
 main()
